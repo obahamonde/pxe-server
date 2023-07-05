b062ac6e1b64c8bfacb5df32ca955f9da2a06541
@obahamonde <oscarmartinbahamondemunoz@gmail.com>
2023-07-04 21:45:50 -0500

    fixed dnsmasq.conf

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM ubuntu:20.04
 RUN apt-get update && apt-get install -y dnsmasq
+RUN mkdir -p /var/tftpboot
 COPY dnsmasq.conf /etc/dnsmasq.conf
 EXPOSE 67/udp 69/udp
 CMD ["dnsmasq", "-d"]
--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 docker build -t pxe-server .
-docker run -d --network=host -v ./:/pxe-server pxe-server
+docker network create --driver bridge isolated_network
+
+docker run -d --network=isolated_network -v ./:/pxe-server pxe-server
